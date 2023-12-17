4cd3252cce31f74e7aff08ac15a6da31ea30f71d
Natalie Burgei <natalieburgei04@gmail.com>
2023-12-17 17:32:42 +0300

    completed asssignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        return [item for item in words if sorted(self.word) == sorted(item.lower()) and item.lower() != self.word]
